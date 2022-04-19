d71ea00f12fb42584f3aeaea1a4e93ec595dbb8e
MrRoose <dfaroos@gmail.com>
2022-04-19 18:03:05 +0200

    Added tests folder.

--- tests/inversion_tests.py
+++ tests/inversion_tests.py
@@ -1,7 +1,11 @@
-from main import NormalizingAutoEncoder, RealNVP, Encoder, Decoder
 import torch
 
-def verify_inverse_nae(nae: NormalizingAutoEncoder, num_channels=3, eps=1e-6):
+from flows.realnvp import RealNVP
+from models.autoencoder import Encoder, Decoder
+from models.normalizing_autoencoder import NormalizingAutoEncoder
+
+
+def test_inverse_nae(nae: NormalizingAutoEncoder, num_channels=3, eps=1e-6):
     input = torch.rand(4, num_channels, 28, 28)
     core, shell = nae.partition(input)
     z, deviations, _ = nae.embedding(core, shell)
@@ -9,7 +13,7 @@ def verify_inverse_nae(nae: NormalizingAutoEncoder, num_channels=3, eps=1e-6):
     returned_input = nae.inverse_partition(returned_core, returned_shell)
     return torch.sum(torch.abs(input - returned_input) >= eps) == 0
 
-def verify_forward_nae(nae: NormalizingAutoEncoder, num_channels=3, eps=1e-6):
+def test_forward_nae(nae: NormalizingAutoEncoder, num_channels=3, eps=1e-6):
     input = torch.rand(4, num_channels, 28, 28)
     z, deviations = nae.partition(input)
     core, shell = nae.forward(z, deviations)
@@ -17,7 +21,7 @@ def verify_forward_nae(nae: NormalizingAutoEncoder, num_channels=3, eps=1e-6):
     returned_input = nae.inverse_partition(returned_z, returned_deviations)
     return torch.sum(torch.abs(input - returned_input) >= eps) == 0
 
-def verify_inverse_flow(flow, eps=1e-6):
+def test_inverse_flow(flow, eps=1e-6):
     input = torch.rand(4, flow.input_dim)
     output = flow.forward(flow.inverse(input)[0])
     return torch.sum(torch.abs(input - output) >= eps) == 0
@@ -28,6 +32,6 @@ decoder = Decoder(64, 12, [3,28,28])
 mask = torch.zeros(28,28)
 mask[13:15,13:15] = 1
 nae = NormalizingAutoEncoder(core_flow, encoder, decoder, mask)
-print(f"NAE (inverse -> forward) returns input: {verify_inverse_nae(nae)}")
-print(f"NAE (forward -> inverse) returns input: {verify_forward_nae(nae)}")
-print(f"RealNVP returns input: {verify_inverse_flow(core_flow)}")
+print(f"NAE (inverse -> forward) returns input: {test_inverse_nae(nae)}")
+print(f"NAE (forward -> inverse) returns input: {test_forward_nae(nae)}")
+print(f"RealNVP returns input: {test_inverse_flow(core_flow)}")
