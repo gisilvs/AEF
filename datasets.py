a87e03415951c34b82496a9ff4db432ac1de8a07
MrRoose <dfaroos@gmail.com>
2022-04-20 11:11:31 +0200

    Added datasets.py, refactored main.py to work with datasets.py.

---
+++ datasets.py
@@ -0,0 +1,57 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST, EMNIST, FashionMNIST
+
+
+def get_transform(dataset: str = 'mnist'):
+    img_transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    return img_transform
+
+def get_train_val_dataloaders(dataset: str = 'mnist', batch_size: int = 128, p_validation: float = 0.1, seed: int = 3):
+    '''
+    Returns a train dataloader and a validation dataloader if p_validation > 0.
+    :param dataset: dataset to retrieve. Either "mnist", "emnist" or "fashionmnist".
+    :param batch_size: batch size for each dataloader.
+    :param p_validation: percentage of original training set to take for validation
+    :param seed: manual seed for reproducability
+    :return: (train_dataloader, val_dataloader) if p_validation > 0 else train_dataloader
+    '''
+
+    assert (p_validation >= 0) and (p_validation <= 1)
+
+    img_transform = get_transform(dataset)
+
+    if dataset.lower() == 'mnist':
+        train_dataset = MNIST(root='./data/MNIST', download=True, train=True, transform=img_transform)
+    elif dataset.lower() == 'emnist':
+        train_dataset = EMNIST(root='./data/MNIST', download=True, train=True, transform=img_transform)
+    elif dataset.lower() == 'fashionmnist':
+        train_dataset = FashionMNIST(root='./data/MNIST', download=True, train=True, transform=img_transform)
+
+    if p_validation > 0:
+        torch_rng = torch.Generator().manual_seed(seed)
+        size_validation = round(p_validation * len(train_dataset))
+        size_train = len(train_dataset) - size_validation
+        train_subset, val_subset = torch.utils.data.random_split(train_dataset, [size_train, size_validation],
+                                                             generator=torch_rng)
+        train_dataloader = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
+        validation_dataloader = DataLoader(val_subset, batch_size=batch_size, shuffle=False)
+        return train_dataloader, validation_dataloader
+    else:
+        return DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+def get_test_dataloader(dataset: str = 'mnist', batch_size: int = 128):
+
+    img_transform = get_transform(dataset)
+
+    if dataset.lower() == 'mnist':
+        test_dataset = MNIST(root='./data/MNIST', download=True, train=False, transform=img_transform)
+    elif dataset.lower() == 'emnist':
+        test_dataset = EMNIST(root='./data/MNIST', download=True, train=False, transform=img_transform)
+    elif dataset.lower() == 'fashionmnist':
+        test_dataset = FashionMNIST(root='./data/MNIST', download=True, train=False, transform=img_transform)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return test_dataloader
