f5c416d6a6c02ce8ebadee67c65832e356d13595
MrRoose <dfaroos@gmail.com>
2022-04-22 10:46:14 +0200

    Added seed to DatasetLoaders.

--- datasets.py
+++ datasets.py
@@ -10,6 +10,12 @@ def get_transform(dataset: str = 'mnist'):
     ])
     return img_transform
 
+
+def get_list_of_datasets():
+    lst = ['mnist', 'emnist', 'fashionmnist', 'cifar10']
+    return lst
+
+
 def get_train_val_dataloaders(dataset: str = 'mnist', batch_size: int = 128, p_validation: float = 0.1, seed: int = 3,
                               return_img_dim: bool = True, return_alpha: bool = True):
     '''
@@ -19,12 +25,14 @@ def get_train_val_dataloaders(dataset: str = 'mnist', batch_size: int = 128, p_v
     :param p_validation: percentage of original training set to take for validation
     :param seed: manual seed for reproducibility
     :param return_img_dim: whether to return the img dimensions
+    :param return_alpha: whether to return the alpha corresponding to a dataset (for conversion to logit space)
     :return: train_dataloader, val_dataloader (if p_validation > 0), img_dim (if return_img_dim), alpha (if return_alpha)
     '''
 
     assert (p_validation >= 0) and (p_validation <= 1)
 
     img_transform = get_transform(dataset)
+    torch_rng = torch.Generator().manual_seed(seed)
 
     if dataset.lower() == 'mnist':
         train_dataset = MNIST(root='./data/MNIST', download=True, train=True, transform=img_transform)
@@ -40,17 +48,16 @@ def get_train_val_dataloaders(dataset: str = 'mnist', batch_size: int = 128, p_v
         alpha = .05
 
     if p_validation > 0:
-        torch_rng = torch.Generator().manual_seed(seed)
         size_validation = round(p_validation * len(train_dataset))
         size_train = len(train_dataset) - size_validation
         train_subset, val_subset = torch.utils.data.random_split(train_dataset, [size_train, size_validation],
                                                              generator=torch_rng)
-        train_dataloader = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
-        validation_dataloader = DataLoader(val_subset, batch_size=batch_size, shuffle=False)
+        train_dataloader = DataLoader(train_subset, batch_size=batch_size, shuffle=True, generator=torch_rng)
+        validation_dataloader = DataLoader(val_subset, batch_size=batch_size, shuffle=False, generator=torch_rng)
 
         return_tuple = [train_dataloader, validation_dataloader]
     else:
-        return_tuple = [DataLoader(train_dataset, batch_size=batch_size, shuffle=True)]
+        return_tuple = [DataLoader(train_dataset, batch_size=batch_size, shuffle=True, generator=torch_rng)]
 
     if return_img_dim:
         img_dim = train_dataset[0][0].shape
@@ -59,6 +66,7 @@ def get_train_val_dataloaders(dataset: str = 'mnist', batch_size: int = 128, p_v
         return_tuple.append(alpha)
 
     return return_tuple
+
 
 def get_test_dataloader(dataset: str = 'mnist', batch_size: int = 128):
 
