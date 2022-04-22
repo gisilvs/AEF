9ad402a5df5400f03d7576a44fb08335da37d37b
MrRoose <dfaroos@gmail.com>
2022-04-22 11:38:51 +0200

    Added KMNIST. Fixed paths for the different datasets.

--- datasets.py
+++ datasets.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.datasets import MNIST, EMNIST, FashionMNIST, CIFAR10
+from torchvision.datasets import MNIST, EMNIST, FashionMNIST, CIFAR10, KMNIST
 
 
 def get_transform(dataset: str = 'mnist'):
@@ -12,7 +12,7 @@ def get_transform(dataset: str = 'mnist'):
 
 
 def get_list_of_datasets():
-    lst = ['mnist', 'emnist', 'fashionmnist', 'cifar10']
+    lst = ['mnist', 'kmnist', 'emnist', 'fashionmnist', 'cifar10']
     return lst
 
 
@@ -37,14 +37,17 @@ def get_train_val_dataloaders(dataset: str = 'mnist', batch_size: int = 128, p_v
     if dataset.lower() == 'mnist':
         train_dataset = MNIST(root='./data/MNIST', download=True, train=True, transform=img_transform)
         alpha = 1e-6
+    elif dataset.lower() == 'kmnist':
+        train_dataset = KMNIST(root='./data/KMNIST', download=True, train=True, transform=img_transform)
+        alpha = 1e-6
     elif dataset.lower() == 'emnist':
-        train_dataset = EMNIST(root='./data/MNIST', download=True, train=True, transform=img_transform)
+        train_dataset = EMNIST(root='./data/EMNIST', split='letters', download=True, train=True, transform=img_transform)
         alpha = 1e-6
     elif dataset.lower() == 'fashionmnist':
-        train_dataset = FashionMNIST(root='./data/MNIST', download=True, train=True, transform=img_transform)
+        train_dataset = FashionMNIST(root='./data/FashionMNIST', download=True, train=True, transform=img_transform)
         alpha = 1e-6
     elif dataset.lower() == 'cifar10' or dataset.lower() == 'cifar':
-        train_dataset = CIFAR10(root='./data/MNIST', download=True, train=True, transform=img_transform)
+        train_dataset = CIFAR10(root='./data/CIFAR', download=True, train=True, transform=img_transform)
         alpha = .05
 
     if p_validation > 0:
@@ -74,12 +77,14 @@ def get_test_dataloader(dataset: str = 'mnist', batch_size: int = 128):
 
     if dataset.lower() == 'mnist':
         test_dataset = MNIST(root='./data/MNIST', download=True, train=False, transform=img_transform)
+    elif dataset.lower() == 'kmnist':
+        test_dataset = KMNIST(root='./data/KMNIST', download=True, train=False, transform=img_transform)
     elif dataset.lower() == 'emnist':
-        test_dataset = EMNIST(root='./data/MNIST', download=True, train=False, transform=img_transform)
+        test_dataset = EMNIST(root='./data/EMNIST', split='letters', download=True, train=False, transform=img_transform)
     elif dataset.lower() == 'fashionmnist':
-        test_dataset = FashionMNIST(root='./data/MNIST', download=True, train=False, transform=img_transform)
+        test_dataset = FashionMNIST(root='./data/FashionMNIST', download=True, train=False, transform=img_transform)
     elif dataset.lower() == 'cifar10' or dataset.lower() == 'cifar':
-        test_dataset = CIFAR10(root='./data/MNIST', download=True, train=False, transform=img_transform)
+        test_dataset = CIFAR10(root='./data/CIFAR', download=True, train=False, transform=img_transform)
 
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     return test_dataloader
