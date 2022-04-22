049da638bbb5f545a49f60c736dd69ec47ceb408
MrRoose <dfaroos@gmail.com>
2022-04-22 12:17:02 +0200

    Added test for equality of validation batches.

--- tests/dataset_tests.py
+++ tests/dataset_tests.py
@@ -1,3 +1,5 @@
+import torch
+
 from models.autoencoder_base import AutoEncoder
 from datasets import get_train_val_dataloaders, get_list_of_datasets, get_test_dataloader
 from models.vae import VAE
@@ -21,4 +23,23 @@ def test_processing_all_datasets():
         _ = model.loss_function(test_batch)
     return True
 
+def test_equality_validation_batches(n_to_check: int = 5):
+    dataset_names = get_list_of_datasets()
+
+    for dataset_name in dataset_names:
+        _, val_dataloader, _, _ = get_train_val_dataloaders(dataset_name)
+        _, val_dataloader_2, _, _ = get_train_val_dataloaders(dataset_name)
+        val_dataloader_iterator_2 = iter(val_dataloader_2)
+        n_checked = 0
+        for val_batch, _ in val_dataloader:
+            val_batch_2, _ = next(val_dataloader_iterator_2)
+            if not torch.allclose(val_batch, val_batch_2, atol=1e-6):
+                return False
+            n_checked += 1
+            if n_checked >= n_to_check:
+                break
+    return True
+
+
 print(f"test_processing_all_datasets: {'Success' if test_processing_all_datasets() else 'Fail'}")
+print(f"test_equality_validation_batches: {'Success' if test_equality_validation_batches() else 'Fail'}")
