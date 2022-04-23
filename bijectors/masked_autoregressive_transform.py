415a972c4a92fd3614fd4c55296e30a9f8b957ab
gisilvs <gianlu.silvestri@gmail.com>
2022-04-23 15:53:23 +0200

    implemented masked autoregressive flow

--- bijectors/masked_autoregressive_transform.py
+++ bijectors/masked_autoregressive_transform.py
@@ -1,16 +1,15 @@
 """Implementations of autoregressive bijectors."""
 
-from torch.nn import functional as F
-
-from nflows.distributions.normal import StandardNormal
-from nflows.flows.base import Flow
 from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform
 from nflows.transforms.base import CompositeTransform
-from nflows.transforms.permutations import RandomPermutation, ReversePermutation
-from bijectors.actnorm import ActNorm
 from nflows.transforms.base import InverseTransform
+from nflows.transforms.permutations import RandomPermutation, ReversePermutation
+from torch.nn import functional as F
 
-#todo: check which is the right direction for maf and iaf
+from bijectors.actnorm import ActNorm
+
+
+# todo: check which is the right direction for maf and iaf
 def get_autoregressive_flow(
         features,
         hidden_features,
@@ -24,7 +23,6 @@ def get_autoregressive_flow(
         batch_norm_within_layers=False,
         act_norm_between_layers=False,
         is_inverse=False):
-
     if use_random_permutations:
         permutation_constructor = RandomPermutation
     else:
