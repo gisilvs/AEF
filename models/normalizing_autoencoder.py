9082356ef625616775f1c666b24f9028a2cbff60
mrroose <dfaroos@gmail.com>
2022-04-26 19:12:33 +0200

    Further work on visualization.

--- models/normalizing_autoencoder.py
+++ models/normalizing_autoencoder.py
@@ -154,15 +154,13 @@ class NormalizingAutoEncoder(GaussianAutoEncoder):
 
     def sample(self, num_samples: int = 1, sample_deviations: bool = False, temperature: float = 1, z: Tensor = None):
         device = self.get_device()
-
+        deviations = None
         if z is None:
             z = torch.normal(torch.zeros(num_samples, self.core_size),
                              torch.ones(num_samples, self.core_size)*temperature).to(device)
             if sample_deviations:
                 deviations = torch.normal(torch.zeros_like(self.mask),
                                           torch.ones_like(self.mask)*temperature).to(device)
-            else:
-                deviations = None
 
         core, shell = self.forward(z, deviations)
         y = self.inverse_partition(core, shell)
--- visualize.py
+++ visualize.py
@@ -1,8 +1,11 @@
 import torch.distributions
+import torchvision
 
 import datasets
+import util
 import wandb
 from models.models import get_model
+import matplotlib.pyplot as plt
 
 
 # def get_model_by_run_id(project: str, run_id: str, run_name:str, which_file: str = 'best', wandb_api: wandb.apis.public.Api = None):
@@ -30,6 +33,35 @@ from models.models import get_model
 # run_id = '3hho6lhe'
 # get
 
-def get_z_values(n_vals: int = 20, border: float = 0.15, distr: torch.distributions.Distribution, latent_dims: int = 2):
+
+
+def get_z_values(n_vals: int = 20, border: float = 0.15, latent_dims: int = 2):
     lin_vals = torch.linspace(border, 1-border, steps=n_vals)
-    grid = torch.meshgrid(*[lin_vals]*latent_dims)
+    icdf_vals = torch.cartesian_prod(*([lin_vals]*latent_dims))
+    distr = torch.distributions.normal.Normal(torch.zeros(latent_dims), torch.ones(latent_dims))
+    z_vals = distr.icdf(icdf_vals)
+
+    return z_vals
+
+from util import load_best_model
+
+run = wandb.init()
+project_name = 'prototyping'
+model_name = 'nae'
+experiment_name = 'vae_mnist_run_0_latent_size_2_decoder_fixed'
+latent_dims = 2
+image_dim = [1, 28, 28]
+alpha = 1e-6
+use_center_pixels = True
+device = 'cpu'
+decoder = 'fixed'
+
+model = load_best_model(run, project_name, model_name, experiment_name, device, latent_dims, image_dim, alpha, decoder,
+                        use_center_pixels, version='best:v14')
+z_vals = get_z_values(n_vals=10, latent_dims=2)
+
+output = model.sample(z=z_vals).detach()
+
+util.plot_image_grid(output, 10, 10, 1)
+plt.show()
+run.finish()
