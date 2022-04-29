d680419f28d41f49d81495390824d6f2ae65d2c9
gisilvs <gianlu.silvestri@gmail.com>
2022-04-29 14:56:09 +0200

    added eval and no grad

--- fid_example.py
+++ fid_example.py
@@ -22,7 +22,7 @@ device = torch.device("cuda:0" if use_gpu and torch.cuda.is_available() else "cp
 dataset = 'mnist'
 #model_name = 'nae-center'
 model_name = 'vae'
-latent_size = 8
+latent_size = 32
 latent_dims = latent_size
 
 if model_name == 'nae-corner':
@@ -38,7 +38,7 @@ elif model_name == 'nae-center':
     model_name_addition = '-center'
     use_center_pixels = True
 else:
-    experiment_name = f'{model_name}_{dataset}_run_2_latent_size_{latent_size}_decoder_fixed'
+    experiment_name = f'{model_name}_{dataset}_run_4_latent_size_{latent_size}_decoder_fixed'
     decoder = 'fixed'
     model_name_addition = ''  # This could be nicer
 experiment_name += '_best'
@@ -46,7 +46,8 @@ model = load_best_model(run, project_name, model_name, experiment_name, device, 
                             alpha, use_center_pixels, version='latest')
 
 model = model.to(device)
-metrics.calculate_fid(model, dataset, device, n_samples=32)
+model.eval()
+with torch.no_grad():
+    metrics.calculate_fid(model, dataset, device, 2048)
 run.finish()
 exit()
-
