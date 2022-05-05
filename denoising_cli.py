Fix encode/decode for existing models. Add log marginal calculation to models itself. Refactor get_model such that vae with small architecture can take prior/posterior flow.

denoising_cli.py
@@ -96,7 +96,7 @@ for run_nr in args.runs:
     if not os.path.isdir('./checkpoints'):
         os.mkdir('./checkpoints')
 
-    run = wandb.init(project="denoising-experiments", entity="nae",
+    run = wandb.init(project="denoising", entity="nae",
                      name=run_name, config=config)
     wandb.summary['n_parameters'] = count_parameters(model)
     print('Training ...')
@@ -209,6 +209,7 @@ for run_nr in args.runs:
                         if n_iterations_done == 0:
                             best_loss = validation_reconstruction_errors[-1]
                             best_it = n_iterations_done
+                            torch.save(model.state_dict(), f'checkpoints/{run_name}_best.pt')
                         # For VAE-like models we save based on validation likelihood
                         elif validation_losses[-1] < best_loss:
                             best_loss = validation_losses[-1]
@@ -315,12 +316,8 @@ for run_nr in args.runs:
                 test_batch = dequantize(test_batch)
                 test_batch = test_batch.to(device)
                 for iw_iter in range(20):
-                    log_likelihood = vae_log_prob(model, test_batch, n_samples=128)
-                    if model_name in flow_like_models:
-                        loss = torch.mean(model.loss_function(test_batch))
-                    else:
-                        loss = torch.mean(model.loss_function(test_batch, test_batch))
-                    test_ll_averager(loss.item())
+                    log_likelihood = torch.mean(model.approximate_marginal(test_batch, n_samples=128))
+                    test_ll_averager(log_likelihood.item())
             test_ll = test_ll_averager(None)
             wandb.summary['test_log_likelihood'] = test_ll
             bpp_test = bits_per_pixel(test_ll, n_pixels)
@@ -366,7 +363,7 @@ for run_nr in args.runs:
     plt.close("all")
 
     # Clean up older artifacts
-    api = wandb.Api(overrides={"project": 'denoising-experiments', "entity": "nae"})
+    api = wandb.Api(overrides={"project": 'denoising', "entity": "nae"})
     artifact_type, artifact_name = 'model', f'{run_name}_latest'
     for version in api.artifact_versions(artifact_type, artifact_name):
         if len(version.aliases) == 0:

main_cli.py
@@ -65,9 +65,9 @@ prior_flow = args.prior_flow
 args.runs = [int(item) for item in args.runs.split(',')]
 
 AE_like_models = ['nae-center', 'nae-corner', 'nae-external', 'vae', 'iwae', 'vae-iaf']
-dirs = os.listdir(args.data_dir)
-for d in dirs:
-    print(d)
+# dirs = os.listdir(args.data_dir)
+# for d in dirs:
+#     print(d)
 for run_nr in args.runs:
     if args.custom_name is not None:
         run_name = args.custom_name
@@ -145,7 +145,7 @@ for run_nr in args.runs:
 
                 optimizer.zero_grad()
                 loss.backward()
-                torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=200.)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=200.)
                 optimizer.step()
 
                 # We validate first iteration, every n iterations, and at the last iteration
@@ -242,9 +242,8 @@ for run_nr in args.runs:
                 test_batch = dequantize(test_batch)
                 test_batch = test_batch.to(device)
                 for iw_iter in range(20):
-                    log_likelihood = vae_log_prob(model, test_batch, n_samples=128)
-                    loss = torch.mean(model.loss_function(test_batch))
-                    test_ll_averager(loss.item())
+                    log_likelihood = torch.mean(model.approximate_marginal(test_batch, n_samples=128))
+                    test_ll_averager(log_likelihood.item())
             test_ll = test_ll_averager(None)
             wandb.summary['test_log_likelihood'] = test_ll
             bpp_test = bits_per_pixel(test_ll, n_pixels)

models/autoencoder_base.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn, Tensor, distributions
-
 from nflows.transforms import Transform, IdentityTransform
 
 from models.autoencoder import GaussianEncoder, GaussianDecoder
@@ -52,6 +51,18 @@ class GaussianAutoEncoder(AutoEncoder):
         z = self.prior.sample((num_samples, )) * temperature
         return self.decode(z)[0]
 
+    def approximate_marginal(self, images: Tensor, n_samples: int = 128):
+        batch_size = images.shape[0]
+        mu_z, sigma_z = self.encoder(images)
+        samples = distributions.normal.Normal(mu_z, sigma_z).sample([n_samples]).transpose(1, 0)
+        mu_x, sigma_x = self.decoder(samples.reshape(batch_size * n_samples, -1))
+        mu_x, sigma_x = mu_x.view(batch_size, n_samples, -1), sigma_x.view(batch_size, n_samples, -1)
+        p_x_z = distributions.normal.Normal(mu_x, sigma_x).log_prob(images.view(batch_size, 1, -1)).sum([2]).view(batch_size, n_samples)
+        p_latent = distributions.normal.Normal(0, 1).log_prob(samples).sum([-1])
+        q_latent = distributions.normal.Normal(mu_z.unsqueeze(1), sigma_z.unsqueeze(1)).log_prob(samples).sum([-1])
+
+        return torch.mean(torch.logsumexp(p_x_z + p_latent - q_latent, [1]) - torch.log(torch.tensor(n_samples)))
+
     def set_device(self):
         # TODO: override to? see https://stackoverflow.com/questions/59179609/how-to-make-a-pytorch-distribution-on-gpu
         if self.device is None:
@@ -59,6 +70,7 @@ class GaussianAutoEncoder(AutoEncoder):
             # Putting loc and scale to device gives nans for some reason
             self.prior = distributions.normal.Normal(torch.zeros(self.latent_dims).to(self.device),
                                                      torch.ones(self.latent_dims).to(self.device))
+
 
 class ExtendedGaussianAutoEncoder(GaussianAutoEncoder):
     def __init__(self, encoder: GaussianEncoder, decoder: GaussianDecoder, 
@@ -71,9 +83,23 @@ class ExtendedGaussianAutoEncoder(GaussianAutoEncoder):
         self.set_device()
         z0 = self.prior.sample((num_samples,)) * temperature
         z, _ = self.prior_bijector.forward(z0)
-        return self.decode(z)[0]
+        return self.decoder(z)[0]
 
     def encode(self, x: Tensor):
         z0 = self.encoder(x)[0]
         z = self.posterior_bijector.forward(z0)[0]
         return z
+
+    def approximate_marginal(self, images: Tensor, n_samples: int = 128):
+        batch_size = images.shape[0]
+        mu_z, sigma_z = self.encoder(images)
+        z0_samples = distributions.normal.Normal(mu_z, sigma_z).sample([n_samples]).transpose(1, 0)
+        z_samples, _ = self.posterior_bijector.forward(z0_samples.reshape(batch_size * n_samples, -1))
+        mu_x, sigma_x = self.decoder(z_samples)
+        z_samples = z_samples.view(batch_size, n_samples, -1)
+        mu_x, sigma_x = mu_x.view(batch_size, n_samples, -1), sigma_x.view(batch_size, n_samples, -1)
+        p_x_z = distributions.normal.Normal(mu_x, sigma_x).log_prob(images.view(batch_size, 1, -1)).sum([2]).view(batch_size, n_samples)
+        p_latent = distributions.normal.Normal(0, 1).log_prob(z_samples).sum([-1])
+        q_latent = distributions.normal.Normal(mu_z.unsqueeze(1), sigma_z.unsqueeze(1)).log_prob(z0_samples).sum([-1])
+
+        return torch.mean(torch.logsumexp(p_x_z + p_latent - q_latent, [1]) - torch.log(torch.tensor(n_samples)))

models/vae_iaf.py
@@ -14,14 +14,14 @@ class VAEIAF(GaussianAutoEncoder):
 
     def forward(self, x: Tensor):
         self.set_device()
-        z_mu, z_sigma = self.encode(x)
+        z_mu, z_sigma = self.encoder(x)
         z0 = distributions.normal.Normal(z_mu, z_sigma + self.eps).rsample()
         z, log_j_z = self.iaf.forward(z0)
-        x_mu, x_sigma = self.decode(z)
+        x_mu, x_sigma = self.decoder(z)
         return x_mu, x_sigma, z_mu, z_sigma, z0, log_j_z, z
 
     def encode(self, x: Tensor):
-        z0 = self.encoder(x)
+        z0 = self.encoder(x)[0]
         z = self.iaf.forward(z0)
         return z
 
@@ -32,6 +32,39 @@ class VAEIAF(GaussianAutoEncoder):
         q_z = distributions.normal.Normal(z_mu, z_sigma + self.eps).log_prob(z0).sum(-1) - log_j_z
         p_z = distributions.normal.Normal(0, 1).log_prob(z).sum(-1)
         return -(reconstruction_loss + p_z - q_z)
+
+    def approximate_marginal(self, images: Tensor, n_samples: int = 128):
+    #     batch_size = x.shape[0]
+    #     z_mu, z_sigma = self.encoder(x)
+    #     z = Normal(z_mu, z_sigma + self.eps).rsample([self.num_samples]).transpose(1, 0)
+    #     x_mu, x_sigma = self.decoder(z.reshape(batch_size * self.num_samples, -1))
+    #     x_mu, x_sigma = x_mu.view(batch_size, self.num_samples, -1), x_sigma.view(batch_size, self.num_samples, -1)
+    #     return x_mu, x_sigma, z_mu, z_sigma, z
+    #
+    # def loss_function(self, x: Tensor):
+    #     self.set_device()
+    #     batch_size = x.shape[0]
+    #     x_mu, x_sigma, z_mu, z_sigma, z = self.forward(x)
+    #     p_x_z = Normal(x_mu, x_sigma + self.eps).log_prob(x.view(batch_size, 1, -1)).sum([2]).view(batch_size,
+    #                                                                                                self.num_samples)
+    #     p_latent = self.prior.log_prob(z).sum([-1])
+    #     q_latent = Normal(z_mu.unsqueeze(1), z_sigma.unsqueeze(1) + self.eps).log_prob(z).sum([-1])
+    #
+    #     return -torch.mean(
+    #         torch.logsumexp(p_x_z + p_latent - q_latent, [1]) - torch.log(torch.tensor(self.num_samples)))
+
+        batch_size = images.shape[0]
+        mu_z, sigma_z = self.encoder(images)
+        z0_samples = distributions.normal.Normal(mu_z, sigma_z).sample([n_samples]).transpose(1, 0)
+        z_samples, _ = self.iaf.forward(z0_samples.reshape(batch_size * n_samples, -1))
+        mu_x, sigma_x = self.decoder(z_samples)
+        z_samples = z_samples.view(batch_size, n_samples, -1)
+        mu_x, sigma_x = mu_x.view(batch_size, n_samples, -1), sigma_x.view(batch_size, n_samples, -1)
+        p_x_z = distributions.normal.Normal(mu_x, sigma_x).log_prob(images.view(batch_size, 1, -1)).sum([2]).view(batch_size, n_samples)
+        p_latent = distributions.normal.Normal(0, 1).log_prob(z_samples).sum([-1])
+        q_latent = distributions.normal.Normal(mu_z.unsqueeze(1), sigma_z.unsqueeze(1)).log_prob(z0_samples).sum([-1])
+
+        return torch.mean(torch.logsumexp(p_x_z + p_latent - q_latent, [1]) - torch.log(torch.tensor(n_samples)))
 
 
 class ExtendedVAEIAF(ExtendedVAE):

visualize.py
@@ -151,7 +151,10 @@ def plot_reconstructions(model: GaussianAutoEncoder, test_loader: DataLoader, de
 
             row_batch = row_batch.to(device)
             with torch.no_grad():
-                z, _ = model.encode(row_batch)
+
+                z = model.encode(row_batch)
+                if isinstance(z, tuple):
+                    z = z[0]
                 reconstruction = model.decode(z)
                 # NAE returns a single value, VAEs will return mu and sigma
                 if isinstance(reconstruction, tuple):
@@ -363,7 +366,7 @@ def generate_visualizations_separately():
     # generate_visualizations(False, True, False, False, 0, custom_name="2D latent space plots")
     # generate_visualizations(False, False, True, False, 0, custom_name="Latent grid plots")
     # generate_visualizations(False, False, False, True, 5, custom_name="Samples")
-    generate_visualizations(False, False, False, False, 5, True, 1, custom_name="Reconstructions")
+    generate_visualizations(False, False, False, False, 5, True, 1, custom_name="Reconstructions_new")
 
 
 def generate_visualizations(do_plot_latent_space_greater_than_2=False,