25a4fa8991084bdb2a8683863448047237bc534e
gisilvs <gianlu.silvestri@gmail.com>
2022-07-28 22:48:54 +0200

    further reduced model size

--- models/vdvae.py
+++ models/vdvae.py
@@ -17,10 +17,10 @@ def get_encoder_string(image_dim: List, latent_ndims: int, size: str = None):
         if size == 'test':
             enc_block_str = "32x1,32d2,16x1,16d2,8x1,8d2,4x1,4d4,1x3"
         else:
-            enc_block_str = "32x8,32d2,16x4,16d2,8x4,8d2,4x2,4d4,1x2"
+            enc_block_str = "32x4,32d2,16x4,16d2,8x4,8d2,4x4,4d4,1x4"
             #enc_block_str = "32x8,32d2,16x4,16d2,8x4,8d2,4x2,4d4,1x2"
     elif image_dim == [3, 64, 64]:
-        enc_block_str = "64x8,64d2,32x6,32d2,16x4,16d2,8x4,8d2,4x2,4d4,1x2"
+        enc_block_str = "64x4,64d2,32x4,32d2,16x4,16d2,8x4,8d2,4x4,4d4,1x4"
 # # This code doesn't support 28x28
     # if image_dim == [1, 28, 28]:
     #     enc_block_str = "28x6,28d2,14x4,14d2,7x3,7d2,3x3,3d2,1x2"
@@ -33,10 +33,10 @@ def get_decoder_string(image_dim: List, latent_ndims: int, size: str = None):
         if size == 'test':
             dec_block_str = "1x1,4m1,4x1,8m4,8x1,16m8,16x1,32m16,32x1"
         else:
-            dec_block_str = "1x2,4m1,4x2,8m4,8x4,16m8,16x4,32m16,32x8"
+            dec_block_str = "1x4,4m1,4x4,8m4,8x4,16m8,16x4,32m16,32x4"
             #dec_block_str = "1x2,4m1,4x2,8m4,8x4,16m8,16x4,32m16,32x8"
     elif image_dim == [3, 64, 64]:
-        dec_block_str = "1x2,4m1,4x2,8m4,8x4,16m8,16x4,32m16,32x6,64m32,64x8"
+        dec_block_str = "1x4,4m1,4x4,8m4,8x4,16m8,16x4,32m16,32x4,64m32,64x4"
     # # This code doesn't support 28x28
     # if image_dim == [1, 28, 28]:
     #     dec_block_str = "1x1,4m1,4x2,8m4,8x4,14m8,14x6,28m14,28x14"
